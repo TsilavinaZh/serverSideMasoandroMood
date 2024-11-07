fe4cfcee218153f0392c7c40296f1343baf10417
Tsilavina <159104790+TsilavinaZh@users.noreply.github.com>
2024-11-07 14:36:51 +0200

    Add files via upload

--- test.js
+++ test.js
@@ -46,6 +46,11 @@ app.post('/api/contact', (req) => {
   <p style="font-size: 18px; font-weight: bold; color: #4a47ff; margin-bottom: 10px;">Email: <span style="font-weight: normal; color: #555;">${email}</span></p>
   <p style="font-size: 18px; font-weight: bold; color: #4a47ff; margin-bottom: 10px;">Message: <span style="font-weight: normal; color: #555;">${message}</span></p>
 </div>
+
+<footer style="background-color: #333; color: #fff; text-align: center; padding: 20px 0; position: relative; bottom: 0; width: 100%;">
+  <p style="font-size: 16px; margin: 0;">© 2024 Masoandro Mood</p>
+</footer>
+
       `,
       category: "Commande",
     })
@@ -55,8 +60,39 @@ app.post('/api/contact', (req) => {
 
 app.post('/api/catalogue', (req, res) => {
   const receivedData = req.body;
-  appendDataToFile('catalogue donnee', receivedData);
+
+  // Assurez-vous que `receivedData` est bien un objet et formatez-le si nécessaire
+  const formattedData = JSON.stringify(receivedData, null, 2);
+
+  transport
+    .sendMail({
+      from: sender,
+      to: recipients,
+      subject: "Commande",
+      html: `
+        <h1 style="text-align: center; color: #4a47ff;">Masoandro Mood</h1>
+<h2 style="text-align: center; color: #555;">Détails de la commande:</h2>
+        <pre>${formattedData}</pre>
+
+        <footer style="background-color: #333; color: #fff; text-align: center; padding: 20px 0; position: relative; bottom: 0; width: 100%;">
+  <p style="font-size: 16px; margin: 0;">© 2024 Masoandro Mood</p>
+</footer>
+      `,
+      category: "Commande",
+    })
+    .then(
+      () => {
+        // Envoi réussi
+        res.status(200).send("Commande envoyée avec succès.");
+      },
+      (error) => {
+        // En cas d'erreur
+        console.error("Erreur d'envoi du mail:", error);
+        res.status(500).send("Erreur lors de l'envoi de la commande.");
+      }
+    );
 });
+
 
 app.listen(port, () => {
   console.log(`Serveur en écoute sur http://localhost:${port}`);
